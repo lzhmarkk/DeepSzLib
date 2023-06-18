cca7fba44c65bc7d8ac4f9bf33e388e58db8aaf3
lzhmark <lzhmarkk@163.com>
2023-06-18 11:16:53 +0800

    update segmentation

--- models/utils.py
+++ models/utils.py
@@ -17,21 +17,21 @@ class Segmentation(nn.Module):
             self.linear2.append(nn.Linear(self.dim, self.S))
 
     def segment(self, x):
-        T, B, C = x.shape
-        x = x.reshape(self.S, -1, B, C).permute(1, 2, 3, 0)  # (T', B, C, S)
+        B, T, C = x.shape
+        x = x.reshape(B, -1, self.S, C).permute(0, 1, 3, 2)  # (B, T', C, S)
 
         x_emb = []
         for c in range(self.channels):
-            x_emb.append(self.linear1[c](x[:, :, c, :]))  # (T', B, D)
+            x_emb.append(self.linear1[c](x[:, :, c, :]))  # (B, T', D)
         x_emb = torch.stack(x_emb, dim=2)
-        return x_emb
+        return x_emb  # (B, T', C, D)
 
     def unsegment(self, x_emb):
-        T, B, C, D = x_emb.shape
+        B, T, C, D = x_emb.shape
 
         x = []
         for c in range(self.channels):
-            x.append(self.linear2[c](x_emb[:, :, c, :]))  # (T', B, S)
-        x = torch.stack(x, dim=2)  # (T', B, C, S)
-        x = x.premute(3, 0, 1, 2).reshape(-1, B, C)
+            x.append(self.linear2[c](x_emb[:, :, c, :]))  # (B, T', S)
+        x = torch.stack(x, dim=2)  # (B, T', C, S)
+        x = x.permute(0, 1, 3, 2).reshape(B, -1, C)
         return x
