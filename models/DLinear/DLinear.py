ff02dec6e91fa08a9a9a2f9f411459a623cb6d36
lzhmark <lzhmarkk@163.com>
2023-07-06 15:09:03 +0800

    update DLinear

--- models/DLinear/DLinear.py
+++ models/DLinear/DLinear.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from models.DLinear.utils import series_decomp
+from models.utils import Segmentation
 
 
 class DLinear(nn.Module):
@@ -10,14 +11,22 @@ class DLinear(nn.Module):
 
     def __init__(self, args):
         super(DLinear, self).__init__()
-        self.seq_len = args.window
-        self.pred_len = args.hidden
-        assert args.preprocess != 'seg' and args.preprocess != 'fft'
+        self.seq_len = args.window // args.seg
+        self.pred_len = 32
+        # assert args.preprocess != 'seg' and args.preprocess != 'fft'
+        self.preprocess = args.preprocess
+        self.seg = args.seg
 
         # Decompsition Kernel Size
         self.decompsition = series_decomp(args.kernel_size)
         self.individual = args.individual
         self.channels = args.n_channels
+
+        if self.preprocess == 'seg':
+            self.dim = self.pred_len
+            self.segmentation = Segmentation(self.seg, self.dim, self.channels)
+        elif self.preprocess == 'fft':
+            self.dim = self.seg // 2
 
         if self.individual:
             self.Linear_Seasonal = nn.ModuleList()
@@ -27,29 +36,15 @@ class DLinear(nn.Module):
                 self.Linear_Seasonal.append(nn.Linear(self.seq_len, self.pred_len))
                 self.Linear_Trend.append(nn.Linear(self.seq_len, self.pred_len))
 
-                # Use this two lines if you want to visualize the weights
-                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
         else:
             self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
             self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
 
-            # Use this two lines if you want to visualize the weights
-            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-
-        if args.act == 'tanh':
-            self.act = nn.Tanh()
-        elif args.act == 'relu':
-            self.act = nn.ReLU()
-        else:
-            self.act = nn.Identity()
-
-        self.Linear_Channel = nn.Sequential(nn.Linear(self.pred_len * self.channels, self.pred_len),
-                                            nn.GELU(),
-                                            nn.Linear(self.pred_len, self.pred_len * self.channels))
-        self.norm = nn.LayerNorm(self.pred_len * self.channels)
-        self.fc = nn.Linear(self.pred_len * self.channels, 1)
+        self.decoder = nn.Sequential(nn.Linear(self.pred_len * self.dim, self.pred_len),
+                                     nn.GELU(),
+                                     nn.Linear(self.pred_len, 1))
+        self.Linear_Dim = nn.Linear(self.dim, self.dim)
+        self.Linear_Channel = nn.Linear(self.channels, 1)
 
     def forward(self, x):
         # (B, T, C, D/S)
@@ -57,11 +52,15 @@ class DLinear(nn.Module):
         x = x.squeeze(dim=-1)
 
         # x: [Batch, Input length, Channel]
+        if self.preprocess == 'seg':
+            x = self.segmentation.segment(x)  # (B, T, C, D)
+
+        x = self.Linear_Dim(x)
         seasonal_init, trend_init = self.decompsition(x)
-        seasonal_init, trend_init = seasonal_init.permute(0, 2, 1), trend_init.permute(0, 2, 1)
+        seasonal_init, trend_init = seasonal_init.permute(0, 2, 3, 1), trend_init.permute(0, 2, 3, 1)
         if self.individual:
-            seasonal_output = torch.zeros([seasonal_init.size(0), seasonal_init.size(1), self.pred_len], dtype=seasonal_init.dtype).to(seasonal_init.device)
-            trend_output = torch.zeros([trend_init.size(0), trend_init.size(1), self.pred_len], dtype=trend_init.dtype).to(trend_init.device)
+            seasonal_output = torch.zeros([seasonal_init.size(0), seasonal_init.size(1), self.dim, self.pred_len], dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0), trend_init.size(1), self.dim, self.pred_len], dtype=trend_init.dtype).to(trend_init.device)
             for i in range(self.channels):
                 seasonal_output[:, i, :] = self.Linear_Seasonal[i](seasonal_init[:, i, :])
                 trend_output[:, i, :] = self.Linear_Trend[i](trend_init[:, i, :])
@@ -69,10 +68,7 @@ class DLinear(nn.Module):
             seasonal_output = self.Linear_Seasonal(seasonal_init)
             trend_output = self.Linear_Trend(trend_init)
 
-        x = seasonal_output + trend_output
-
-        x = self.act(x)
-        x = x.reshape(bs, -1)  # (B, C*T)
-        x = self.norm(self.Linear_Channel(x) + x)
-        x = self.fc(x).squeeze()
+        x = seasonal_output + trend_output  # (B, C, D, T)
+        x = self.decoder(x.reshape(bs, self.channels, -1)).squeeze(-1)  # (B, C)
+        x = self.Linear_Channel(x).squeeze(-1)  # (B)
         return x
--- models/DLinear/utils.py
+++ models/DLinear/utils.py
@@ -10,15 +10,15 @@ class moving_avg(nn.Module):
     def __init__(self, kernel_size, stride):
         super(moving_avg, self).__init__()
         self.kernel_size = kernel_size
-        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+        self.avg = nn.AvgPool2d(kernel_size=(kernel_size, 1), stride=stride, padding=0)
 
     def forward(self, x):
         # padding on the both ends of time series
-        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1, 1)
         x = torch.cat([front, x, end], dim=1)
-        x = self.avg(x.permute(0, 2, 1))
-        x = x.permute(0, 2, 1)
+        x = self.avg(x.permute(0, 2, 1, 3))
+        x = x.permute(0, 2, 1, 3)
         return x
 
 
