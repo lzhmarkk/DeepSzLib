4eee83b5538260c7e3458e186b8281edfbdb15be
lzhmark <lzhmarkk@163.com>
2023-07-13 16:40:43 +0800

    multi-task Transformer

--- models/Transformer/Transformer.py
+++ models/Transformer/Transformer.py
@@ -8,6 +8,7 @@ class Transformer(nn.Module):
         super().__init__()
         self.seg = args.seg
         self.window = args.window
+        self.horizon = args.horizon
         self.hidden = args.hidden
         self.layers = args.layers
         self.channels = args.n_channels
@@ -15,6 +16,7 @@ class Transformer(nn.Module):
         self.dropout = args.dropout
         self.position_encoding = args.pos_enc
         self.preprocess = args.preprocess
+        self.multi_task = args.multi_task
 
         if self.preprocess == 'seg':
             self.dim = self.hidden
@@ -35,6 +37,13 @@ class Transformer(nn.Module):
 
         self.cls_token = nn.Parameter(torch.randn(1, 1, self.channels, self.hidden), requires_grad=True)
 
+        if self.multi_task:
+            self.pred_pos_emb = nn.Parameter(torch.randn([self.horizon // self.seg, self.channels, self.hidden]), requires_grad=True)
+
+            transformer_decoder_layer = nn.TransformerDecoderLayer(self.hidden, self.heads, 4 * self.hidden, self.dropout)
+            self.pred_decoder = nn.TransformerDecoder(transformer_decoder_layer, self.layers)
+            self.pred_fc = nn.Linear(self.hidden, self.dim)
+
     def forward(self, x):
         # (B, T, C, D/S)
         bs = x.shape[0]
@@ -50,10 +59,22 @@ class Transformer(nn.Module):
             x = x + self.pos_emb.unsqueeze(0)  # (B, 1+T, C, D)
 
         x = x.permute(1, 0, 2, 3).reshape(1 + self.window // self.seg, bs * self.channels, self.hidden)  # (1+T, B*C, D)
-        z = self.encoder(x)  # (1+T, B*C, D)
-        z = z[0, :, :]  # (B*C, D)
+        h = self.encoder(x)  # (1+T, B*C, D)
+
+        # decoder
+        z = h[0, :, :]  # (B*C, D)
         z = z.reshape(bs, self.channels * self.hidden)  # (B, C*D)
 
         z = torch.tanh(z)
         z = self.decoder(z).squeeze()  # (B)
-        return z
+
+        if not self.multi_task:
+            return z
+        else:
+            m = h[1:, :, :]  # (T, B*C, D)
+            y = self.pred_pos_emb.unsqueeze(dim=1).repeat(1, bs, 1, 1).reshape(self.horizon // self.seg, bs * self.channels, self.hidden)
+            y = self.pred_decoder(y, m)
+            y = y.reshape(self.horizon // self.seg, bs, self.channels, self.hidden)  # (T, B, C, D)
+            y = self.pred_fc(y)
+            y = y.permute(1, 0, 2, 3)  # (B, T, C, D)
+            return z, y
