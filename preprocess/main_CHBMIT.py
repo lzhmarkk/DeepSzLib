f4e5fe59054f05aa7ca8a59a3ca30421ba0aa17b
lzhmark <lzhmarkk@163.com>
2023-08-30 02:34:59 +0800

    update CHBMIT preprocessing

--- preprocess/main_CHBMIT.py
+++ preprocess/main_CHBMIT.py
@@ -3,16 +3,16 @@ import re
 import mne
 import json
 import numpy as np
-from tqdm import tqdm
 from scipy.signal import resample
 from process import process
 
 origin_dir = f"./data/original_dataset/CHBMIT/1.0.0"
 dest_dir = f"./data/CHBMIT/"
 channels = ["FP1-F7", "F7-T7", "T7-P7", "P7-O1", "FP1-F3", "F3-C3", "C3-P3", "P3-O1", "FP2-F4", "F4-C4", "C4-P4", "P4-O2", "FP2-F8",
-            "F8-T8", "T8-P8", "P8-O2", "FZ-CZ", "CZ-PZ", "P7-T7", "T7-FT9", "FT9-FT10", "FT10-T8"]
+            "F8-T8", "T8-P8", "P8-O2", "FZ-CZ", "CZ-PZ"]
 n_sample_per_file = 1000
 np.random.seed(0)
+drop_non_seizure_files = True
 
 
 def load_edf_data(edf_path, sample_rate):
@@ -25,18 +25,16 @@ def load_edf_data(edf_path, sample_rate):
             new_data.append(data[c][0])
         elif c + '-0' in data.ch_names:
             new_data.append(data[c + '-0'][0])
-        else:
-            # print(f"Channel {c} not in {edf_path}")
-            new_data.append(np.zeros([1, len(data)]))
 
     new_data = np.concatenate(new_data, axis=0).T  # (T, C)
+    assert new_data.shape[1] == len(channels), f"{edf_path} misses channels"
+
     resample_data = resample(new_data, num=new_data.shape[0] // orig_sample_rate * sample_rate, axis=0)
     return resample_data
 
 
 def load_truth_data(extracted_info, length, sample_rate):
     truth = np.zeros([length], dtype=float)
-    # print(extracted_info)
 
     timestamps = extracted_info['timestamp']
     assert len(timestamps) == 2 * int(extracted_info['Number of Seizures in File'])
@@ -84,6 +82,7 @@ def load_summary(txt_path):
                     info_dict[key.strip()] = value.strip()
 
                 assert current_channels is not None
+                assert len(timestamps) == 2 * int(info_dict['Number of Seizures in File'])
                 info_dict['timestamp'] = timestamps
                 info_dict['Channels'] = current_channels
                 extracted_info[info_dict['File Name']] = info_dict
@@ -105,40 +104,56 @@ if __name__ == '__main__':
         stride = config["stride"]
         seg = config["seg"]
 
+    with open(os.path.join(origin_dir, 'RECORDS-WITH-SEIZURES'), 'r') as fp:
+        files_with_seizures = fp.readlines()
+        files_with_seizures = [file.strip().split('/')[1] for file in files_with_seizures if '.edf' in file]
+        print(files_with_seizures)
+
     # load data
     user_id = 0
     all_x, all_y, all_channels = [], [], []
     patient_dirs = list(filter(lambda p: 'chb' in p and os.path.isdir(os.path.join(origin_dir, p)), os.listdir(origin_dir)))
     for patient_dir in patient_dirs:
-        user_id = str(patient_dir[-2:])
-        print(patient_dir)
+        print("*" * 30 + patient_dir + "*" * 30)
+        user_id = str(patient_dir[3:])
         _all_x, _all_y, _all_channels = [], [], []
         patient_dir = os.path.join(origin_dir, patient_dir)
-        sample_rate, extracted_info = load_summary(os.path.join(patient_dir, f"chb{user_id}-summary.txt"))
+        _, extracted_info = load_summary(os.path.join(patient_dir, f"chb{user_id}-summary.txt"))
 
+        skip_files = []
         edf_files = list(filter(lambda f: os.path.splitext(f)[1] == '.edf', os.listdir(patient_dir)))
-        for edf_file in tqdm(edf_files):
-            # edf_file=f"{edf_file.split('.')[0]}.edf"
-            x = load_edf_data(os.path.join(patient_dir, edf_file), sample_rate)
+        for edf_file in edf_files:
+            try:
+                x = load_edf_data(os.path.join(patient_dir, edf_file), sample_rate)
 
-            if edf_file in extracted_info:
-                y = load_truth_data(extracted_info[edf_file], length=x.shape[0], sample_rate=sample_rate)
+                if edf_file in extracted_info:
+                    y = load_truth_data(extracted_info[edf_file], length=x.shape[0], sample_rate=sample_rate)
+                else:
+                    y = np.zeros([x.shape[0]], dtype=float)
+
+                if edf_file in files_with_seizures:
+                    assert y.any(), f"File {edf_file} should be a seizure file, but found no seizures"
+                else:
+                    assert (~y.astype(bool)).all(), f"File {edf_file} should be a non-seizure file, but found seizures"
+
+            except Exception as e:
+                skip_files.append(edf_file)
+                print(f"{edf_file}: Error {repr(e)}")
+
             else:
-                y = np.zeros([x.shape[0]], dtype=float)
+                x = x[:len(x) // (window * sample_rate) * (window * sample_rate)]
+                y = y[:len(x) // (window * sample_rate) * (window * sample_rate)]
 
-            invalid_channels = np.all(x == 0, axis=0)
-            if invalid_channels.any():
-                print(f"File {edf_file} misses {np.sum(invalid_channels)} channels {np.array(channels)[invalid_channels]}")
+                # drop files that do not have seizures
+                if drop_non_seizure_files and (~y.astype(bool)).all():
+                    print(f"{edf_file}: Drop since it has no seizures")
+                    continue
 
-            if (~invalid_channels).any():
+                print(f"{edf_file}: Loaded")
+
                 _all_x.append(x)
                 _all_y.append(y)
-            else:
-                print(f"Skip file {edf_file}")
 
-        # trimming
-        _all_x = [x[:x.shape[0] // (100 * window) * (100 * window)] for x in _all_x]
-        _all_y = [y[:y.shape[0] // (100 * window) * (100 * window)] for y in _all_y]
         all_x.append(np.concatenate(_all_x, axis=0))
         all_y.append(np.concatenate(_all_y, axis=0))
 
