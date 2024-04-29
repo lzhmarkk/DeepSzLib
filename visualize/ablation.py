932f47285121c9114ca69f2ace50d4d97123be49
lzhmark <lzh01832123@alibaba-inc.com>
2024-04-29 11:16:08 +0800

    update ablation & efficiency

--- visualize/ablation.py
+++ visualize/ablation.py
@@ -3,18 +3,18 @@ from collections import OrderedDict
 
 results = {
     "FDUSZ": {
-        "w/o SE": [0.543, 0.569, 0.876],
-        "w/o DyAN": [0.609, 0.606, 0.912],
-        # "w/o RNN": [0.621, 0.606, 0.910],
-        "w/o ESM": [0.581, 0.590, 0.899],
-        "w/o ACL": [0.620, 0.623, 0.910],
-        "w/o DCL": [0.608, 0.610, 0.906],
-        "full DSN": [0.632, 0.641, 0.915]
+        "w/o SE": [0.533, 0.549, 0.866],
+        "w/o RAN": [0.616, 0.611, 0.914],
+        "w/o RNN": [0.621, 0.606, 0.910],
+        "w/o ESM": [0.578, 0.584, 0.899],
+        "w/o ACL": [0.614, 0.616, 0.907],
+        "w/o DCL": [0.600, 0.606, 0.902],
+        "full DSN": [0.633, 0.620, 0.915]
     },
     "TUSZ": {
         "w/o SE": [0.654, 0.645, 0.947],
-        "w/o DyAN": [0.719, 0.696, 0.966],
-        # "w/o RNN": [0.715, 0.683, 0.964],
+        "w/o RAN": [0.719, 0.696, 0.966],
+        "w/o RNN": [0.715, 0.683, 0.964],
         "w/o ESM": [0.699, 0.682, 0.961],
         "w/o ACL": [0.720, 0.709, 0.964],
         "w/o DCL": [0.717, 0.695, 0.962],
@@ -22,8 +22,8 @@ results = {
     },
     "CHBMIT": {
         "w/o SE": [0.381, 0.382, 0.842],
-        "w/o DyAN": [0.583, 0.551, 0.947],
-        # "w/o RNN": [0.558, 0.507, 0.944],
+        "w/o RAN": [0.583, 0.551, 0.947],
+        "w/o RNN": [0.558, 0.507, 0.944],
         "w/o ESM": [0.510, 0.423, 0.935],
         "w/o ACL": [0.585, 0.519, 0.944],
         "w/o DCL": [0.552, 0.524, 0.941],
@@ -38,7 +38,6 @@ hatches = ['//', 'x', 'x', 'x', '\\\\', '\\\\', '++']  # '++', '*', 'O', 'o', '.
 fontsize = 20
 
 if __name__ == '__main__':
-    # FDUSZ & TUSZ
     fig = plt.figure(figsize=(12, 5))
     gs = fig.add_gridspec(1, 2)
     axs = gs.subplots(sharey=False)
@@ -90,26 +89,29 @@ if __name__ == '__main__':
                 y = result[variate][j + 2]
                 ax.bar(x, y, width * 0.9, color=colors[k + 1], hatch=hatches[k])
 
-    fig.legend(loc="upper center", fontsize=fontsize, ncols=4, columnspacing=1)
+    fig.legend(loc="upper center", fontsize=fontsize, ncols=5, columnspacing=1)
     fig.tight_layout()
     plt.subplots_adjust(top=0.78, wspace=0.5)
     plt.savefig("./ExpAblation1.png", dpi=500)
     plt.show()
 
-    # CHBMIT
+    # appendix
     fig = plt.figure(figsize=(12, 5))
     gs = fig.add_gridspec(1, 10)
-    axs = plt.subplot(gs[0, 3:7])
+    # axs = gs.subplots(sharey=False)
+    axs = fig.add_subplot(gs[0, 3:7])
+
     datasets = ["CHBMIT"]
     for i, dataset in enumerate(datasets):
         result = OrderedDict(results[dataset])
         ax = axs
         ax.set_xticks(range(3), metrics)
+        ax.set_yticks([0.3, 0.4, 0.5, 0.6])
         ax.tick_params(labelsize=fontsize)
 
         # F1/F2
         ax.set_ylabel('F1 / F2', fontsize=fontsize)
-        ax.set_xlabel(f"({['a', 'b', 'c'][i]}) {dataset}", fontsize=fontsize + 4)
+        ax.set_xlabel(f"(c) {dataset}", fontsize=fontsize + 4)
         if dataset == 'FDUSZ':
             ax.set_ylim(0.5, 0.65)
             ax.set_yticks([0.5, 0.6])
@@ -132,15 +134,22 @@ if __name__ == '__main__':
         ax = ax.twinx()
         ax.set_ylabel(r'AUC', fontsize=fontsize)
         ax.tick_params(labelsize=fontsize)
-        ax.set_ylim(0.8, 0.97)
-        ax.set_yticks([0.8, 0.9])
+        if dataset == 'FDUSZ':
+            ax.set_ylim(0.85, 0.93)
+            ax.set_yticks([0.85, 0.9])
+        elif dataset == 'TUSZ':
+            ax.set_ylim(0.94, 0.97)
+            ax.set_yticks([0.94, 0.95, 0.96])
+        else:
+            ax.set_ylim(0.8, 0.97)
+            ax.set_yticks([0.8, 0.9])
         for j, metric in enumerate(metrics[2:]):
             for k, variate in enumerate(result):
                 x = j + 2 + (k - len(result) / 2) * width
                 y = result[variate][j + 2]
                 ax.bar(x, y, width * 0.9, color=colors[k + 1], hatch=hatches[k])
 
-    fig.legend(loc="upper center", fontsize=fontsize, ncols=4, columnspacing=1)
+    fig.legend(loc="upper center", fontsize=fontsize, ncols=5, columnspacing=1)
     fig.tight_layout()
     plt.subplots_adjust(top=0.78, wspace=0.5)
     plt.savefig("./ExpAblation2.png", dpi=500)
--- visualize/efficiency.py
+++ visualize/efficiency.py
@@ -1,33 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-models = ["SegRNN", "STGCN", "CNN-LSTM", "DCRNN-dist", "TSD",
-          "LTransformer", "CrossFormer", "SageFormer", "DeepSOZ", "DSN"]
-online = ["SegRNN", "CNN-LSTM", "DCRNN-dist", "LTransformer", "DSN"]
+models = ["STGCN", "SegRNN", "MTGNN", "CNN-LSTM", "TSD", "DCRNN",
+          "FEDFormer", "LTransformer", "CrossFormer", "SageFormer", "DSN"]
+online = ["SegRNN", "CNN-LSTM", "DCRNN", "LTransformer", "DSN"]
 window = [15, 30, 45, 60, 90, 120]
 a = 9604
-markers = {"SegRNN": '.', "CNN-LSTM": "v", "DCRNN-dist": "s", "LTransformer": "*", "DSN": "D",
-           "CNN": ",", "STGCN": "1", "MTGNN": 'p', "TSD": '+', 'DeepSOZ': 'x', 'CrossFormer': '<', 'SageFormer': '>'}
+markers = {"SegRNN": '.', "CNN-LSTM": "v", "DCRNN": "s", "LTransformer": "*", "DSN": "D",
+           "CNN": ",", "STGCN": "1", "MTGNN": 'p', "TSD": '+', 'FEDFormer': 'x', 'CrossFormer': '<', 'SageFormer': '>'}
 cmap = plt.colormaps.get_cmap('tab20').colors
-colors = {"SegRNN": cmap[0], "CNN-LSTM": cmap[2], "DCRNN-dist": cmap[4], "LTransformer": cmap[8], "DSN": cmap[6],
-          "CNN": cmap[10], "STGCN": cmap[12], "MTGNN": cmap[14], "TSD": cmap[16], 'DeepSOZ': cmap[18], 'CrossFormer': cmap[1],
+colors = {"SegRNN": cmap[0], "CNN-LSTM": cmap[2], "DCRNN": cmap[4], "LTransformer": cmap[8], "DSN": cmap[6],
+          "CNN": cmap[10], "STGCN": cmap[12], "MTGNN": cmap[14], "TSD": cmap[16], 'FEDFormer': cmap[18], 'CrossFormer': cmap[1],
           'SageFormer': cmap[3]}
 
 detection_time = {"SegRNN": [2.20, 4.11, 5.80, 6.79, 10.28, 12.73],
                   "STGCN": [2.74, 4.41, 6.65, 7.82, 10.59, 13.49],
                   "MTGNN": [3.32, 5.02, 6.04, 8.69, 12.48, 16.07],
                   "CNN-LSTM": [3.10, 5.61, 6.84, 8.46, 12.40, 15.71],
-                  "DCRNN-dist": [3.29, 4.61, 6.85, 8.15, 10.07, 17.83],
+                  "DCRNN": [3.29, 4.61, 6.85, 8.15, 10.07, 17.83],
                   "TSD": [2.80, 4.60, 5.93, 7.36, 13.53, 16.37],
                   "LTransformer": [3.16, 4.22, 6.31, 7.89, 12.91, 17.00],
-                  "DeepSOZ": [3.21, 5.32, 6.44, 8.58, 12.44, 15.89],
+                  "FEDFormer": [3.13, 4.42, 6.57, 7.83, 12.06, 16.93],
                   "CrossFormer": [3.16, 5.07, 6.67, 8.66, 12.44, 17.40],
                   "SageFormer": [3.22, 5.05, 6.23, 8.24, 12.17, 15.88],
                   "DSN": [3.12, 5.53, 7.98, 10.32, 15.09, 19.82]}
 
 onset = {"SegRNN": [2.20, 4.11, 5.80, 6.79, 10.28, 12.73],
          "CNN-LSTM": [3.10, 5.61, 6.84, 8.46, 12.40, 15.71],
-         "DCRNN-dist": [3.29, 4.61, 6.85, 8.15, 10.07, 17.83],
+         "DCRNN": [3.29, 4.61, 6.85, 8.15, 10.07, 17.83],
          "LTransformer": [3.16, 4.22, 6.31, 7.89, 12.91, 17.00],
          "DSN": [3.12, 5.53, 7.98, 10.32, 15.09, 19.82]}
 
@@ -35,10 +35,10 @@ memory = {"SegRNN": [857, 917, 957, 999, 1083, 1169],
           "STGCN": [875, 947, 1045, 1125, 1289, 1453],
           "MTGNN": [1085, 1219, 1445, 1673, 2131, 2595],
           "CNN-LSTM": [1541, 2273, 2991, 3709, 5151, 6589],
-          "DCRNN-dist": [905, 1001, 1081, 1117, 1257, 1481],
+          "DCRNN": [905, 1001, 1081, 1117, 1257, 1481],
           "TSD": [839, 865, 891, 933, 1007, 1091],
           "LTransformer": [837, 853, 887, 911, 957, 1019],
-          "DeepSOZ": [804, 1066, 1328, 1590, 2116, 2652],
+          "FEDFormer": [1147, 1639, 1887, 2867, 4053, 4815],
           "CrossFormer": [1101, 1535, 2049, 2683, 3359, 5007],
           "SageFormer": [1075, 1323, 1567, 2087, 2491, 4395],
           "DSN": [1223, 1311, 1579, 1771, 2125, 2455]}
@@ -47,10 +47,10 @@ onset_memory = {"SegRNN": [831, 831, 831, 831, 831, 831],
                 "STGCN": [875, 947, 1045, 1125, 1289, 1453],
                 "MTGNN": [1085, 1219, 1445, 1673, 2131, 2595],
                 "CNN-LSTM": [877, 877, 877, 877, 877, 877],
-                "DCRNN-dist": [857, 857, 857, 857, 857, 857],
+                "DCRNN": [857, 857, 857, 857, 857, 857],
                 "TSD": [839, 865, 891, 933, 1007, 1091],
                 "LTransformer": [799, 799, 799, 799, 799, 799],
-                "DeepSOZ": [804, 1066, 1328, 1590, 2116, 2652],
+                "FEDFormer": [1147, 1639, 1887, 2867, 4453, 4815],
                 "CrossFormer": [1101, 1535, 2049, 2683, 3359, 5007],
                 "SageFormer": [1075, 1323, 1567, 2087, 2491, 4395],
                 "DSN": [1129, 1129, 1129, 1129, 1129, 1129]}
@@ -63,14 +63,14 @@ if __name__ == '__main__':
     onset_time = {}
     for m in models:
         if m in online:
-            tp = a / np.array([detection_time[m][5]] * 6) * 120
+            tp = a / np.array(detection_time[m]) * window
         else:
             tp = a / np.array(detection_time[m])
         throughput[m] = tp
         onset_time[m] = total_seconds / tp
 
     # plot
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    fig, axs = plt.subplots(2, 2, figsize=(10, 12))
 
     # detection time
     axs[0, 0].set_xlabel("Window Length (s)\n(a)", fontsize=fontsize)
@@ -101,8 +101,8 @@ if __name__ == '__main__':
     for m in models:
         axs[1, 1].plot(window, onset_memory[m], marker=markers[m], color=colors[m])
 
-    fig.legend(loc="upper center", fontsize=16, ncols=5, columnspacing=0.6)
+    fig.legend(loc="upper center", fontsize=fontsize-2, ncols=4, columnspacing=1)
     fig.tight_layout()
-    plt.subplots_adjust(top=0.9)
+    plt.subplots_adjust(top=0.88, hspace=0.4)
     plt.savefig("./ExpEfficiency.png", dpi=500)
     plt.show()
